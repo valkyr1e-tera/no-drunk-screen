4779ae83ecc425acdb99f9256abc6d0b8ce9f775
hugedong69 <pentagonalpentagon@gmail.com>
2017-08-03 16:37:13 +0800

    Added more effects
    will at all later maybe probably not

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = function noBlurScreen(dispatch) {
     dispatch.hook('sAbnormalityBegin', 2, (event) => {
     //console.log('Abnormality:'+event.id+' Duration:'+event.duration+' Stacks:'+event.stacks)
-    if(event.id == "70237" || event.id == "905434")
+    if(event.id == "48733" || event.id == "48734" || event.id == "48735" || event.id == "48736" || event.id == "48737" || event.id == "48738" || event.id == "48739" || event.id == "70234" || event.id == "70235" ||event.id == "70236" || event.id == "70237" || event.id == "70238" || event.id == "905434")
         {return false;}
       });
 }
